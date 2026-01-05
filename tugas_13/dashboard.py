f36dbf168395b4773d9e6cfb752193aef69bc3ec
WahyuMuhammadFaqih <faqihreo@gmail.com>
2026-01-05 18:56:30 +0700

    Fix CSV path for Streamlit Cloud

--- tugas_13/dashboard.py
+++ tugas_13/dashboard.py
@@ -80,7 +80,7 @@ def create_rfm_df(df):
 # =========================
 # LOAD DATA
 # =========================
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("tugas_13/all_data.csv")
 
 for col in ["order_date", "delivery_date"]:
     all_df[col] = pd.to_datetime(all_df[col])
